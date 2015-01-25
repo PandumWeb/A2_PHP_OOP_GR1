6588396cd2d1bd45a70b087105fa69b0a0accb34
LOR <sorialor@gmail.com>
2015-01-25 19:37:49 +0100

    second commit
    good

--- create_pokemon.php
+++ create_pokemon.php
@@ -13,42 +13,6 @@ use PandumWeb\Init\Pokemon\PokemonPlant;
 
 /** @var \Doctrine\ORM\EntityRepository $trainerRepository */
 $trainerRepository = $em->getRepository('PandumWeb\Init\Trainer');
-// $trainer = $trainerRepository->find($_SESSION['id']);
-
-// $pokemonName = !empty($_POST['pokemonName']) ? $_POST['pokemonName'] : null;
-// $pokemonType = !empty($_POST['pokemonType']) ? $_POST['pokemonType'] : null;
-
-// if (null !== $pokemonName && null !== $pokemonType) {
-//     if ($pokemonType === 'Fire') {
-//         $pokemon = new PokemonFire();
-//         $pokemon
-//             ->setName($pokemonName)
-//             ->setHP(100)
-//             ->setTrainerId($trainer)
-//         ;
-//         $em->persist($pokemon);
-//         $em->flush();
-//     } else if ($pokemonType === 'Water') {
-//         $pokemon = new PokemonWater();
-//         $pokemon
-//             ->setName($pokemonName)
-//             ->setHP(100)
-//             ->setTrainerId($trainer)
-//         ;
-//         $em->persist($pokemon);
-//         $em->flush();
-//     } else {
-//         $pokemon = new PokemonPlant();
-//         $pokemon
-//             ->setName($pokemonName)
-//             ->setHP(100)
-//             ->setTrainerId($trainer)
-//         ;
-//         $em->persist($pokemon);
-//         $em->flush();
-//     }
-//     header('Location: home.php');
-// }
 $trainer = $trainerRepository->find($_SESSION['id']);
 
 $pokemonName = !empty($_POST['pokemonName']) ? $_POST['pokemonName'] : null;
